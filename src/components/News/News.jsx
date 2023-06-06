af41b6cb586774ee1704385c4cae4f82a267e869
Andres Teller <andres.teller2@gmail.com>
2023-06-05 20:12:05 -0500

    Animation to News Cards added

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,23 +1,23 @@
 import { Card, CardBody, CardHeader, Heading, Text } from "@chakra-ui/react";
-import React from "react";
+import { motion } from "framer-motion";
 
 export default function News({title, text}) {
   return (
-    <Card
-      w="27rem"
-      maxH="27rem"
-      textAlign="center"
-      boxShadow="md"
-      borderRadius="2rem"
-    >
-      <CardHeader>
-        <Heading size='lg'>{title}</Heading>
-      </CardHeader>
-      <CardBody>
-        <Text>
-          {text}
-        </Text>
-      </CardBody>
-    </Card>
+    <motion.div whileHover={{ scale: 1.03 }}>
+      <Card
+        w="27rem"
+        maxH="27rem"
+        textAlign="center"
+        boxShadow="md"
+        borderRadius="2rem"
+      >
+        <CardHeader>
+          <Heading size="lg">{title}</Heading>
+        </CardHeader>
+        <CardBody>
+          <Text>{text}</Text>
+        </CardBody>
+      </Card>
+    </motion.div>
   );
 }
